bc8b95472063100c09a8ff43581215dc881d819f
TeimasTeimoso <teimas_2010@hotmail.com>
2021-06-07 23:59:32 +0100

    front page ipm stg. 4

---
+++ MSc/Interfaces Pessoa Máquina/Final Project/Stage 4/gbl-prototype/src/components/BookItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="book">
+        <img :src="require('@/assets/' + cover)" alt="book cover" align="left"/>
+        <br>
+        <p><b>Name:</b>{{ name }}</p>
+        <br>
+        <p><b>Author:</b> {{ author }}</p>
+        <br>
+        <p><b>Year:</b> {{ year }}</p>
+        <br>
+        <p><b>Description:</b> {{ description }}</p>
+        <br>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BookItem',
+    props: {
+        cover: String,
+        name: String,
+        author: String,
+        year: Number,
+        description: String
+    }
+}
+</script>
+
+<style scoped>
+p {
+    text-align: left;
+}
+</style>
---
+++ MSc/Interfaces Pessoa Máquina/Final Project/Stage 4/gbl-prototype/src/views/Books.vue
@@ -0,0 +1,16 @@
+<template>
+    <BookItem cover="Memorial_do_convento.jpg" name='Memorial do Convento' author='José Saramago' year=1982 description='Era uma vez um rei que fez promessa de levantar convento em Mafra. Era uma vez a gente que construiu esse convento. Era uma vez um soldado maneta e uma mulher que tinha poderes. Era uma vez um padre que queria voar e morreu doido.'/>
+    <br>
+    <BookItem cover='clockwork_orange.jpg' name='A Clockwork Orange' author='Anthony Burgess' year=1962 description='In Anthony Burgess influential nightmare vision of the future, criminals take over after dark. Teen gang leader Alex narrates in fantastically inventive slang that echoes the violent intensity of youth rebelling against society. Dazzling and transgressive, A Clockwork Orange is a frightening fable about good and evil and the meaning of human freedom. This edition includes the controversial last chapter not published in the first edition, and Burgess’s introduction, A Clockwork Orange Resucked.'/>
+</template>
+
+<script>
+import BookItem from '@/components/BookItem.vue'
+
+export default {
+    name: 'Books',
+    components: {
+        BookItem
+    }
+}
+</script>
