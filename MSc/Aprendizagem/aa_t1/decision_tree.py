a754a2e2c2df8bbc5d695c346265f5d46837d6a0
TeimasTeimoso <teimas_2010@hotmail.com>
2021-06-26 15:14:46 +0100

    before making shit in A

--- MSc/Aprendizagem/aa_t1/decision_tree.py
+++ MSc/Aprendizagem/aa_t1/decision_tree.py
@@ -2,5 +2,9 @@ from node import Node
 import pandas as pd
 
 class DecisionTree:
-     def __init__(self, dataset:pd.DataFrame) -> None:
-         self.root = Node(dataset)
+     def __init__(self, purity_measure='entropy', prune=False) -> None:
+         self.root = Node()
+         self.purity_measure = purity_measure
+         self.prune = prune
+
+    
--- MSc/Aprendizagem/aa_t1/main.py
+++ MSc/Aprendizagem/aa_t1/main.py
@@ -7,11 +7,6 @@ dataset = pd.read_csv('weather.nominal.csv')
 
 #decision_tree = DecisionTree(dataset)
 attribute_list = list(dataset.columns)[:-1]
-print(attribute_list)
-x_data, y_data = np.split(dataset, [-1], axis=1)
+x_data, y_data = np.split(dataset.values, [-1], axis=1)
 
 x_train, y_train, x_test, y_test = train_test_split(x_data, y_data)
-print(x_train)
-print(y_train)
-print(x_test)
-print(y_test)
