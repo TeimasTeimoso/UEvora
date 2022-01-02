f7180071d079f60162b5a8c824bdc277d0086d6a
TeimasTeimoso <teimas_2010@hotmail.com>
2022-01-02 19:59:50 +0000

    working???

--- MSc/Compressão e Codificação de Dados/Trabalho Final/main.py
+++ MSc/Compressão e Codificação de Dados/Trabalho Final/main.py
@@ -16,18 +16,17 @@ if __name__ == '__main__':
     padded_r = pad_representation(r)
     print(padded_r)
 
+    print("Written:")
     x = h.get_binary_representation(h._root)
     padded_x = pad_representation(x)
     print(padded_x)
 
-    print("Written:")
     with open('b.bin', 'wb') as f:
         b = BitArray(bin=padded_x)
         f.write(b.tobytes())
         f.write(bytes('\n', 'utf-8'))
         bb = BitArray(bin=padded_r)
         f.write(bb.tobytes())
-
 
     #with open("b.bin", "ab") as myfile:
     #    myfile.write(pickle.dumps(h._root))
@@ -38,6 +37,8 @@ if __name__ == '__main__':
     b = BitArray(filename='b.bin')
     print("Read:")
     print(b.bin)
+
+    h.read_tree(b.bin[3:], 0)
 
     #with open('b.txt', 'rb') as f:
     #    a = f.read()
--- MSc/Compressão e Codificação de Dados/Trabalho Final/tree.py
+++ MSc/Compressão e Codificação de Dados/Trabalho Final/tree.py
@@ -1,4 +1,4 @@
-import pickle
+from os import read
 from operator import attrgetter
 from node import Node
 
@@ -38,6 +38,7 @@ class HuffmanTree:
         self._symbol_table[symbol] = computed_code
 
     # Done
+    """
     def get_binary_representation(self, node:Node) -> str:
         left_node_representation = ''
         rigth_node_representation = ''
@@ -56,7 +57,45 @@ class HuffmanTree:
             node_representation = '0'
 
         return left_node_representation + rigth_node_representation + node_representation
+    """
+
+    def get_binary_representation(self, node:Node) -> str:
+        if symbol := node.get_symbol():
+            # doing it this way because of symolbs, but for images it wont be needed
+            ascii_value: int = ord(symbol)
+            bin_ascii_value: str = format(ascii_value, '08b') # 1 byte
+            node_representation = f"1{bin_ascii_value}"
+        else:
+            node_representation = '0'
+
+        if left_node := node.get_left_child():
+            node_representation = node_representation + self.get_binary_representation(left_node)
+        if rigth_node := node.get_rigth_child():
+            node_representation = node_representation + self.get_binary_representation(rigth_node)
+
+        return node_representation
 
     @staticmethod
     def compute_new_probability(left_node: Node, right_node: Node) -> float:
         return left_node.get_probability() + right_node.get_probability()
+
+
+    def get_encoded_data(self, bitstream: str) -> str:
+        number_of_leading_zeros = int(bitstream[:3],2) # get 3 bit representation
+        starting_index = 3+number_of_leading_zeros
+        unpadded_bitstream = bitstream[starting_index:]
+
+    def read_tree(self, unpadded_bitstream: str, index: int) -> Node:
+        if unpadded_bitstream[index] == '1':
+            index += 1
+            bin_symbol = unpadded_bitstream[index:index+8]
+            symbol = chr(int(bin_symbol, 2))
+            return (Node(symbol=symbol), index+7)
+
+        left_node, index = self.read_tree(unpadded_bitstream, index+1)
+        rigth_node, index = self.read_tree(unpadded_bitstream, index+1)
+
+        return (Node(left=left_node, rigth=rigth_node), index)
+
+
+
