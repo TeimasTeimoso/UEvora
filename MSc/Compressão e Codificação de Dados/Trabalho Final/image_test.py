fc8b15412d89cc6f53692abb090043509e37d895
TeimasTeimoso <teimas_2010@hotmail.com>
2022-01-08 00:01:23 +0000

    already have the function that splits the data.

--- MSc/Compressão e Codificação de Dados/Trabalho Final/image_test.py
+++ MSc/Compressão e Codificação de Dados/Trabalho Final/image_test.py
@@ -1,16 +1,6 @@
 from PIL import Image
 import numpy as np
 
-
-# https://stackoverflow.com/questions/11105375/how-to-split-a-matrix-into-4-blocks-using-numpy/11105569
-def split(array, nrows, ncols):
-    """Split a matrix into sub-matrices."""
-
-    _, h = array.shape
-    return (array.reshape(h//nrows, nrows, -1, ncols)
-                 .swapaxes(1, 2)
-                 .reshape(-1, nrows, ncols))
-
 im = Image.open("linda_small.png")
 im_bw = im.convert('1') #convert to black and white
 im_bw.save('linda_small_bw.png')
@@ -21,25 +11,27 @@ print(f"Width: {width} || Heigth: {height}")
 print(len(list(im.getdata())))
 
 im_np = np.array(im).astype(int)
+
 print(im_np)
 
-a=split(im_np, 2,2)
-print((a))
-#print(np.shape(im_np))
-#new_list = split(im_np, 2,2)
-#print('a')
+# https://stackoverflow.com/questions/16856788/slice-2d-array-into-smaller-2d-arrays
+def blockshaped(arr, nrows, ncols):
+    """
+    Return an array of shape (n, nrows, ncols) where
+    n * nrows * ncols = arr.size
 
-# nota => False = 0 = preto (ausencia de cores)
-#      => True = 255 = white (combincação de todas as cores)
-"""
-01 23 45 67 89
-01 23 45 67 89
-01 23 45 67 89
-01 23 45 67 89
-01 23 45 67 89
-01 23 45 67 89
-01 23 45 67 89
-01 23 45 67 89
-01 23 45 67 89
-01 23 45 67 89
-"""
+    If arr is a 2D array, the returned array should look like n subblocks with
+    each subblock preserving the "physical" layout of arr.
+    """
+    h, w = arr.shape
+    return (arr.reshape(h//nrows, nrows, -1, ncols)
+               .swapaxes(1,2)
+               .reshape(-1, nrows, ncols))
+
+a = blockshaped(im_np, 2, 2)
+print(a[12])
+print(a[12].flatten())
+
+test = np.array([1,2,3,4])
+d= test.reshape(2,2)
+print(d.flatten())
