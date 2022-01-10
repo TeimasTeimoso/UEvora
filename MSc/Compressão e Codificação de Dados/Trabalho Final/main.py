0cc9d4d3321102001663ac44ca962ec98a0fbfd2
TeimasTeimoso <teimas_2010@hotmail.com>
2022-01-10 01:59:21 +0000

    its working

--- MSc/Compressão e Codificação de Dados/Trabalho Final/main.py
+++ MSc/Compressão e Codificação de Dados/Trabalho Final/main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from operations.compress import compress_image, write_to_file, open_image, split_into_tiles, tiles_to_symbols
 from operations.extract import extract, extract_dimensions, read_compressed_image, save_extracted_image, join_tiles, unpad_representation
 from data_classes.tree import HuffmanTree
+from utils import compute_average_code_length, compute_entropy
 
 if __name__ == '__main__':
 
@@ -25,7 +26,17 @@ if __name__ == '__main__':
         huffman_tree = HuffmanTree()
         huffman_tree.build_tree(input_sequence)
 
+        average_code_length = compute_average_code_length(input_sequence, huffman_tree._symbol_table)
+        entropy = compute_entropy(input_sequence)
+        print(f"L(C) = {average_code_length}")
+        print(f"H(X) = {entropy}")
+        print(f"[H(X) <= L(C) < H(X) + 1]: {average_code_length - entropy < 1}")
+
         compressed_image = compress_image(input_sequence, huffman_tree._symbol_table)
+
+        original_image_size = heigth * width
+        print(f"Compression rate: {original_image_size/len(compressed_image)}")
+
         write_to_file(file_path, heigth,width, compressed_image, huffman_tree)
     
     elif args.extract:
@@ -39,5 +50,7 @@ if __name__ == '__main__':
         two_dimensional_image = join_tiles(extracted_image, heigth, width)
 
         save_extracted_image(file_path, two_dimensional_image)
+
+        print("Image extracted successfully!")
     else:
         print("Pass --compress or --decompress flags.")
--- MSc/Compressão e Codificação de Dados/Trabalho Final/utils.py
+++ MSc/Compressão e Codificação de Dados/Trabalho Final/utils.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from data_classes.node import Node
+import numpy as np
 
 def get_occurences(input: bytes) -> dict:
     symbol_dict: dict = {}
@@ -19,3 +19,30 @@ def change_file_name(path: Path, extension: str) -> Path:
     str_path_without_ext = str_path.split('.')[0]
     
     return Path(str_path_without_ext+'.'+extension)
+
+def compute_average_code_length(input: str, codes_dict: dict) -> float:
+    average_length: float = 0
+
+    total_chars = len(input)
+    symbol_dict = get_occurences(input)
+
+    for symbol in symbol_dict.keys():
+        occurrences: int = symbol_dict.get(symbol)
+        s_prob = compute_probability(occurrences, total_chars)
+        s_len = len(codes_dict.get(symbol))
+        average_length += s_prob * s_len
+
+    return average_length
+
+def compute_entropy(input: str) -> float:
+    entropy: float = 0
+
+    total_chars = len(input)
+    symbol_dict = get_occurences(input)
+
+    for symbol in symbol_dict.keys():
+        occurrences: int = symbol_dict.get(symbol)
+        s_prob = compute_probability(occurrences, total_chars)
+        entropy += s_prob * np.log2(1/s_prob) 
+
+    return entropy
